a70cde40bafb72b6ba2020df668c94b1038cea0d
wangmaoxiong <2268461750@qq.com>
2020-05-15 19:34:46 +0800

    项目初始化

---
+++ src/main/java/com/wmx/springcloudconfigclientmicros/SpringCloudConfigClientMicrosApplication.java
@@ -0,0 +1,13 @@
+package com.wmx.springcloudconfigclientmicros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigClientMicrosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigClientMicrosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wmx/springcloudconfigclientmicros/SpringCloudConfigClientMicrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wmx.springcloudconfigclientmicros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigClientMicrosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
